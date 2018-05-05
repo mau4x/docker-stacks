57757ec73bff785e9ee0f490fef035c81243d4de
KOREYASU.Masahiro <koreyasu@kurusugawa.jp>
2018-05-05 18:10:30 +0900

    build on cuda-9.1

---
+++ build.sh
@@ -0,0 +1,4 @@
+docker build -t jupyter-cuda/base-notebook base-notebook
+docker build -t jupyter-cuda/minimal-notebook minimal-notebook
+docker build -t jupyter-cuda/scipy-notebook scipy-notebook
+docker build -t jupyter-cuda/datascience-notebook datascience-notebook
